f127a04d298aedda88bef8d4f0d9f8e153660f81
Alexey Efimov <lex.efimov@gmail.com>
2019-08-31 13:30:04 +0500

    Initial Commit

---
+++ MV(X)Petterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MV(X)Petterns
+//
+//  Created by Alexey Efimov on 31/08/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

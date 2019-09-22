9d64bd570b27fd74353d0c33c859a715cbe948bc
Alexey Efimov <lex.efimov@gmail.com>
2019-09-22 21:48:21 +0500

    Clean Swift

---
+++ MV(X)Petterns/Greeting/GreetingConfigurator.swift
@@ -0,0 +1,24 @@
+//
+//  GreetingConfigurator.swift
+//  MV(X)Petterns
+//
+//  Created by Alexey Efimov on 21/09/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+final class GreetingConfigurator {
+    
+    static let shared = GreetingConfigurator()
+    
+    func configure(with view: GreetingViewController) {
+        let viewController = view
+//        let interactor = GreetingInteractor(person: Person(name: "Tim", surname: "Cook"))
+        let interactor = GreetingInteractor()
+        let presenter = GreetingPresenter()
+        viewController.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+    }
+}
